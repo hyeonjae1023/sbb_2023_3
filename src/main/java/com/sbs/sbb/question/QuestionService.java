8153522218bcc07487e9993ed503dd51fe86c240
parkhyeo <hyeonjae1023111@gmail.com>
2023-09-01 10:42:14 +0900

    3-2 ì§ 페이징 진행중

--- src/main/java/com/sbs/sbb/question/QuestionService.java
+++ src/main/java/com/sbs/sbb/question/QuestionService.java
@@ -2,21 +2,18 @@ package com.sbs.sbb.question;
 
 import com.sbs.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class QuestionService {
     private final QuestionRepository questionRepository;
-
-    public List<Question> getList() {
-        return this.questionRepository.findAll();
-    }
-
     public Question getQuestion(Integer id) {
 
         Optional<Question> question = this.questionRepository.findById(id);
@@ -27,12 +24,15 @@ public class QuestionService {
             throw new DataNotFoundException("question not found");
         }
     }
-
     public void create(String subject, String content) {
         Question q = new Question();
         q.setSubject(subject);
         q.setContent(content);
         q.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q);
+    }
+    public Page<Question> getList(int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        return this.questionRepository.findAll(pageable);
     }
 }
--- src/test/java/com/sbs/sbb/SbbApplicationTests.java
+++ src/test/java/com/sbs/sbb/SbbApplicationTests.java
@@ -1,35 +1,37 @@
 package com.sbs.sbb;
 
 import com.sbs.sbb.answer.AnswerRepository;
-import com.sbs.sbb.question.Question;
-import com.sbs.sbb.question.QuestionRepository;
+import com.sbs.sbb.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
-
 @SpringBootTest
 class SbbApplicationTests {
     @Autowired
-    private QuestionRepository questionRepository;
+    private QuestionService questionService;
     @Autowired
     private AnswerRepository answerRepository;
 
 
     @Test
     void testJpa() {
-        Question q1 = new Question();
-        q1.setSubject("sbb가 무엇인가요?");
-        q1.setContent("sbb에 대해서 알고 싶습니다.");
-        q1.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q1);
-
-        Question q2 = new Question();
-        q2.setSubject("스프링 부트 모델 질문입니다.");
-        q2.setContent("id는 자동으로 생성되나요?");
-        q2.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q2);
+        for(int i = 1; i<= 300; i++) {
+            String subject = String.format("테스트 데이터입니다:[%03d]", i);
+            String content = "내용무";
+            this.questionService.create(subject, content);
+        }
+//        Question q1 = new Question();
+//        q1.setSubject("sbb가 무엇인가요?");
+//        q1.setContent("sbb에 대해서 알고 싶습니다.");
+//        q1.setCreateDate(LocalDateTime.now());
+//        this.questionRepository.save(q1);
+//
+//        Question q2 = new Question();
+//        q2.setSubject("스프링 부트 모델 질문입니다.");
+//        q2.setContent("id는 자동으로 생성되나요?");
+//        q2.setCreateDate(LocalDateTime.now());
+//        this.questionRepository.save(q2);
 
 //		List<Question> all = this.questionRepository.findAll();
 //		assertEquals(2, all.size());
