29a2e9174325287aa23cc53758e9897c4cbe0512
parkhyeo <hyeonjae1023111@gmail.com>
2023-09-01 10:07:57 +0900

    2장 완료

--- src/main/java/com/sbs/sbb/question/QuestionController.java
+++ src/main/java/com/sbs/sbb/question/QuestionController.java
@@ -4,7 +4,11 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 @RequestMapping("/question")
@@ -24,9 +28,16 @@ public class QuestionController {
         model.addAttribute("question", question);
         return "question_detail";
     }
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm) {
+        return "question_form";
+    }
     @PostMapping("/create")
-    public String questionCreate(@Valid QuestionForm questionForm, Binding) {
-        this.questionService.create(subject, content);
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            return "question_form";
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list";
     }
 }
